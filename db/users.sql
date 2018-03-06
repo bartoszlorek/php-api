f71a4667ad19526fcb560ef87b67ae9835ec118e
Bartosz Lorek <bery.lorek@gmail.com>
2018-03-06 02:35:31 +0100

    username is not unique

--- db/users.sql
+++ db/users.sql
@@ -2,7 +2,7 @@ CREATE TABLE users
 (
     id          INT NOT NULL AUTO_INCREMENT,
     email       VARCHAR(255) NOT NULL UNIQUE,
-    username    VARCHAR(255) NOT NULL UNIQUE,
+    username    VARCHAR(255) NOT NULL,
     password    VARCHAR(255) NOT NULL,
     role        VARCHAR(255) NOT NULL DEFAULT "user",
     token       VARCHAR(255),
