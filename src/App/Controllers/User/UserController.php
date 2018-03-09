963a9a8e944556e64838eba3bba5bf9934375bab
Bartosz Lorek <bery.lorek@gmail.com>
2018-03-09 23:14:49 +0100

    use resources control and detach pages before deleting user

--- src/App/Controllers/User/UserController.php
+++ src/App/Controllers/User/UserController.php
@@ -6,7 +6,6 @@ use App\Controllers\BaseController;
 use App\Transformers\UserTransformer;
 use App\Exceptions\Error;
 
-use League\Fractal\Resource\Item;
 use Respect\Validation\Validator as v;
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -19,8 +18,8 @@ class UserController extends BaseController {
      */
     public function show(Request $request, Response $response) {
         if ($user = $this->auth->requestUser($request)) {
-            $data = $this->fractal->createData(new Item($user, new UserTransformer()))->toArray();
-            return $this->render($response, $data);
+            $result = $this->resources($user, new UserTransformer);
+            return $this->render($response, $result);
         }
         return Error::unauthorized($response);
     }
@@ -31,22 +30,22 @@ class UserController extends BaseController {
      */
     public function update(Request $request, Response $response) {
         if ($user = $this->auth->requestUser($request)) {
-            $requestData = $this->getParsedBody($request);
-            $validation = $this->validateUpdateRequest($requestData, $user->id);
+            $data = $this->getParsedBody($request);
+            $validation = $this->validateUpdateRequest($data, $user->id);
 
             if ($validation->failed()) {
                 return $this->render($response, $validation->getErrors(), 422);
             }
             $user->update([
-                'email' => isset($requestData['email']) ? $requestData['email'] : $user->email,
-                'username' => isset($requestData['username']) ? $requestData['username'] : $user->username,
-                'password' => isset($requestData['password']) ? password_hash($requestData['password'],
+                'email' => isset($data['email']) ? $data['email'] : $user->email,
+                'username' => isset($data['username']) ? $data['username'] : $user->username,
+                'password' => isset($data['password']) ? password_hash($data['password'],
                     PASSWORD_DEFAULT) : $user->password
             ]);
             // regenerate token after changes
             $user->token = $this->auth->generateToken($user);
-            $data = $this->fractal->createData(new Item($user, new UserTransformer()))->toArray();
-            return $this->render($response, $data);
+            $result = $this->resources($user, new UserTransformer);
+            return $this->render($response, $result);
         }
         return Error::unauthorized($response);
     }
@@ -57,6 +56,7 @@ class UserController extends BaseController {
      */
     public function delete(Request $request, Response $response) {
         if ($user = $this->auth->requestUser($request)) {
+            $user->pages()->detach();
             $user->delete();
             return $this->render($response, 'successfully deleted user', 200);
         }
