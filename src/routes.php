0f26dc11e0b1b74af3724af595286e89030af76c
Bartosz Lorek <bery.lorek@gmail.com>
2018-03-13 01:59:31 +0100

    organize routes

--- src/routes.php
+++ src/routes.php
@@ -12,38 +12,40 @@ use Slim\Http\Response;
 
 // Api Routes
 $app->group('/api', function () {
-    $jwtMiddleware = $this->getContainer()->get('jwt');
-    $optionalAuth = $this->getContainer()->get('optionalAuth');
+    $jwt = $this->getContainer()->get('jwt');
+    $auth = $this->getContainer()->get('optionalAuth');
 
     // Auth Routes
     $this->post('/auth/register', RegisterController::class . ':register')->setName('auth.register');
     $this->post('/auth/login', LoginController::class . ':login')->setName('auth.login');
 
     // User Routes
-    $this->delete('/user', UserController::class . ':delete')->add($jwtMiddleware)->setName('user.delete');
-    $this->get('/user', UserController::class . ':show')->add($jwtMiddleware)->setName('user.show');
-    $this->put('/user', UserController::class . ':update')->add($jwtMiddleware)->setName('user.update');
+    $this->delete('/user', UserController::class . ':delete')->add($jwt)->setName('user.delete');
+    $this->get('/user', UserController::class . ':show')->add($jwt)->setName('user.show');
+    $this->put('/user', UserController::class . ':update')->add($jwt)->setName('user.update');
 
     // Page Routes
-    $this->post('/page', PageController::class . ':create')->add($jwtMiddleware)->setName('page.create');
-    $this->get('/page', PageController::class . ':index')->add($optionalAuth)->setName('page.index');
+    $this->post('/page', PageController::class . ':create')->add($jwt)->setName('page.create');
+    $this->get('/page', PageController::class . ':index')->add($auth)->setName('page.index');
     
-    $this->get('/page/{guid}', PageController::class . ':show')->add($optionalAuth)->setName('page.show');
-    $this->put('/page/{guid}', PageController::class . ':update')->add($jwtMiddleware)->setName('page.update');
-    $this->delete('/page/{guid}', PageController::class . ':delete')->add($jwtMiddleware)->setName('page.delete');
+    $this->get('/page/{guid}', PageController::class . ':show')->add($auth)->setName('page.show');
+    $this->put('/page/{guid}', PageController::class . ':update')->add($jwt)->setName('page.update');
+    $this->delete('/page/{guid}', PageController::class . ':delete')->add($jwt)->setName('page.delete');
 
-    $this->post('/page/{guid}/users', PageController::class . ':attach')->add($jwtMiddleware)->setName('page.attach');
-    $this->delete('/page/{guid}/users', PageController::class . ':detach')->add($jwtMiddleware)->setName('page.detach');
+    $this->post('/page/{guid}/users', PageController::class . ':attach')->add($jwt)->setName('page.attach');
+    $this->delete('/page/{guid}/users', PageController::class . ':detach')->add($jwt)->setName('page.detach');
 
     // Comments
-    $this->post('/page/{guid}/comments', CommentController::class . ':create')->add($jwtMiddleware)->setName('comment.create');
-    $this->get('/page/{guid}/comments', CommentController::class . ':index')->add($optionalAuth)->setName('comment.index');
-    $this->delete('/page/{guid}/comments/{commentId}', CommentController::class . ':delete')->add($jwtMiddleware)->setName('comment.delete');
+    $this->group('/page/{guid}/comments', function () use ($jwt, $auth) {
+        $this->post('', CommentController::class . ':create')->add($jwt)->setName('comment.create');
+        $this->get('', CommentController::class . ':index')->add($auth)->setName('comment.index');
+        $this->delete('/{commentId}', CommentController::class . ':delete')->add($jwt)->setName('comment.delete');
 
-    // Files
-    $this->post('/page/{guid}/comments/{commentId}/files', FileController::class . ':create')->add($jwtMiddleware)->setName('file.create');
-    $this->get('/page/{guid}/comments/{commentId}/files', FileController::class . ':index')->add($optionalAuth)->setName('file.index');
-    $this->delete('/page/{guid}/comments/{commentId}/files/{fileId}', FileController::class . ':delete')->add($jwtMiddleware)->setName('file.delete');
+        // Files
+        $this->post('/{commentId}/files', FileController::class . ':create')->add($jwt)->setName('file.create');
+        $this->get('/{commentId}/files', FileController::class . ':index')->add($auth)->setName('file.index');
+        $this->delete('/{commentId}/files/{fileId}', FileController::class . ':delete')->add($jwt)->setName('file.delete');
+    });
 });
 
 // Routes
