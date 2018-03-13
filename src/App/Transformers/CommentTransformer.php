a036413d4564a8534bdc02875d079e3b75baf4b0
Bartosz Lorek <bery.lorek@gmail.com>
2018-03-13 11:47:02 +0100

    include files in comment transform

--- src/App/Transformers/CommentTransformer.php
+++ src/App/Transformers/CommentTransformer.php
@@ -4,11 +4,12 @@ namespace App\Transformers;
 
 use App\Models\Comment;
 use App\Transformers\UserAttachedTransformer;
+use App\Transformers\FileTransformer;
 use League\Fractal\TransformerAbstract;
 
 class CommentTransformer extends TransformerAbstract {
 
-    protected $defaultIncludes = ['user'];
+    protected $defaultIncludes = ['user', 'files'];
 
     public function transform(Comment $comment) {
         return [
@@ -21,6 +22,10 @@ class CommentTransformer extends TransformerAbstract {
 
     public function includeUser(Comment $comment) {
         return $this->item($comment->user, new UserAttachedTransformer);
+    }
+
+    public function includeFiles(Comment $comment) {
+        return $this->collection($comment->files, new FileTransformer);
     }
 
 }
